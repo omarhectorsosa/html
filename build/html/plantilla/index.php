25ded23e6b09ba28a98931951e7577ea18eed4b8
Omar Sosa <ososa@psa.com.ar>
2023-08-09 13:32:16 -0300

    first commit

---
+++ build/html/plantilla/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE >
+<lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div><a href="http://127.0.0.1:2000/plantilla/1_new-type/form_with_name.html"/> Formulario</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/1_new-type/new_types.html"/> Types</a></div>
+    
+    <div><a href="http://127.0.0.1:2000/plantilla/2_canva/canva_basico.html"/> canva basico</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/2_canva/canva_circulo.html"/> canva circulo</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/2_canva/canva_degradado_circular.html"/> canva Degradado</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/2_canva/canva_gradiente.html"/> canva degradado</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/2_canva/canva_linea.html"/> canva linea</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/2_canva/canva_texto.html"/> canva texto</a></div>
+
+    <div><a href="http://127.0.0.1:2000/plantilla/3_selector/adyacente.html"/>CSS Selector adyacente </a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/3_selector/hermano.html"/>CSS Selector hermano</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/3_selector/hijo.html"/>CSS Selector hijo</a></div>
+
+    <div><a href="http://127.0.0.1:2000/plantilla/4_pseudoclase/first-child.html"/>CSS Pseudoclase </a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/4_pseudoclase/focus.html"/>CSS Pseudoclase </a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/4_pseudoclase/focus-link.html"/>CSS Pseudoclase </a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/4_pseudoclase/last-child.html"/>CSS Pseudoclase </a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/4_pseudoclase/nth-child.html"/>CSS Pseudoclase </a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/4_pseudoclase/nth-child-word.html"/>CSS Pseudoclase </a></div>
+
+    <div><a href="http://127.0.0.1:2000/plantilla/5_pseudo-elemento/after.html"/>CSS Pseudoelemento </a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/5_pseudo-elemento/before.html"/>CSS Pseudoelemento </a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/5_pseudo-elemento/first-letter.html"/>CSS Pseudoelemento </a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/5_pseudo-elemento/first-line.html"/>CSSPseudoelemento </a></div>
+   
+    <div><a href="http://127.0.0.1:2000/plantilla/6_flexbox/flexbox.html"/>CSS Flexbox </a></div> 
+    <div><a href="http://127.0.0.1:2000/plantilla/6_flexbox/flexbox_basis.html"/>CSS Flexbox</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/6_flexbox/flexbox_align.html"/>CSS Flexbox Align</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/6_flexbox/flexbox_flex-direction.html"/>CSS Flexbox direction</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/6_flexbox/flexbox_flex-elemento_flex-grow.html"/>CSS Flexbox flex-grow </a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/6_flexbox/flexbox_flex-elemento_order.html"/>CSS Flexbox order</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/6_flexbox/flexbox_flex-justify.html"/>CSS Flexbox justify</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/6_flexbox/flexbox_flex-wrap.html"/>CSS Flexbox wrap </a></div>
+    
+    <div><a href="http://127.0.0.1:2000/plantilla/7_transiciones/transition.html"/>CSS Transiciones </a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/7_transiciones/transition_size.html"/>CSS Transiciones size </a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/7_transiciones/transition_textbox.html"/>CSS Transiciones texbox</a></div>
+
+    <div><a href="http://127.0.0.1:2000/plantilla/8_animacion/animacion_avance_cuadro.html"/> Animacion avance cuadro</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/8_animacion/animacion_ciclo.html"/> Animacion ciclo</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/8_animacion/animacion_elevator.html"/> Animacion elevador</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/8_animacion/animacion_gradiente.html"/> Animacion gradiente</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/8_animacion/clases.html"/> Animacion clase</a></div>
+    <div><a href="http://127.0.0.1:2000/plantilla/8_animacion/iframe.html"/> Animacion iframe</a></div>
+   
+</body>
+</div>
